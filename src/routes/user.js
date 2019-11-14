e3b4b24cd5c28a22f66d7b0b7f3829fb6aadc580
haffs <abuafeezabolarinwa@yahoo.com>
2019-11-13 17:54:49 -0800

    add signin endpoints

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,10 +4,11 @@ const router = express.Router();
 const userCtrl = require('../controllers/user');
 const authentication = require('../middleware/authentication')
 const per = require('../middleware/checkPermission')
-const { validateCreateUserFormData } = require('../middleware/inputValidation')
+const { validateCreateUserFormData, validateSigninFormData } = require('../middleware/inputValidation')
 
 
 router.post('/create-user', authentication, per.checkPermission('create_user'), validateCreateUserFormData, userCtrl.createUser);
+router.post('/signin', validateSigninFormData, userCtrl.signIn);
 
 
 module.exports = router;
