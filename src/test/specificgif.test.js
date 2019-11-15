6d73b9909143648a6f65042f71b587119958ae0c
haffs <abuafeezabolarinwa@yahoo.com>
2019-11-15 11:57:07 -0800

    add specific gif view rest endpoints

---
+++ src/test/specificgif.test.js
@@ -0,0 +1,58 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../app');
+
+const specficGif = '/api/v1/gifs/11';
+const loginUrl = '/api/v1/auth/signin';
+
+const { expect } = chai;
+chai.use(chaiHttp);
+
+let userToken;
+
+const user = {
+  email: 'john@teamwork.com',
+  password: '123456',
+};
+
+describe('specfic gif post test', () => {
+  before(async () => {
+    const res = await chai.request(app)
+      .post(loginUrl)
+      .send(user);
+    userToken = res.body.payload.token;
+    expect(res).to.have.status(200);
+    expect(res.body.payload).to.have.property('token');
+  });
+  describe('Unauthenticated user can not view specfic gif post.', () => {
+    it('should respond with unauthenticated error', (done) => {
+      chai.request(app)
+        .get(specficGif)
+        .end((err, res) => {
+          expect(res.status).to.equal(401);
+          expect(res.body).to.have.property('success', false);
+          done();
+        });
+    });
+  });
+  describe('User can view specfic gif post', () => {
+    it('should respond with user data and status code 201', (done) => {
+      chai.request(app)
+        .get(specficGif)
+        .set('Authorization', userToken)
+        .end((err, res) => {
+          expect(res.status).to.equal(200);
+          expect(res.body).to.have.property('success', true);
+          expect(res.body.payload).to.have.property('authorid');
+          expect(res.body.payload).to.have.property('firstname');
+          expect(res.body.payload).to.have.property('lastname');
+          expect(res.body.payload).to.have.property('gifid');
+          expect(res.body.payload).to.have.property('gifdate');
+          expect(res.body.payload).to.have.property('giftitle');
+          expect(res.body.payload).to.have.property('image_url');
+          expect(res.body.payload).to.have.property('comments');
+          done();
+        });
+    });
+  });
+});
