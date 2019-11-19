3a09a41b94eefb539cc9227f81e7231ec4357362
haffs <abuafeezabolarinwa@yahoo.com>
2019-11-18 17:21:07 -0800

    update

--- src/models/data.js
+++ src/models/data.js
@@ -9,7 +9,7 @@ const pool = require('./connect');
 const insertInitData = () => {
     const queryText = `INSERT INTO user (first_name, last_name, email, password, gender, job_role, department,address, phone_number, role, create_user, user_access) VALUES  ('Rowling', 'Potter', 'john@teamwork.com', '123456', 'male', 'developer', 'IT', 'ipaja', '090873654375', 'User', null, 'Yes');`
   
-  pool.query(queryText1)
+  pool.query(queryText)
     .then((res) => {
       console.log(res);
       pool.end();
