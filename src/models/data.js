1aa886eba3b43902739e2d1387c1bbb6c029fb3c
haffs <abuafeezabolarinwa@yahoo.com>
2019-11-18 17:12:33 -0800

    add travis database

---
+++ src/models/data.js
@@ -0,0 +1,45 @@
+/* eslint-disable no-console */
+require('dotenv').config();
+
+const pool = require('./connect');
+
+/*
+  Init Data
+*/
+const insertInitData = () => {
+    const queryText = `INSERT INTO user (first_name, last_name, email, password, gender, job_role, department,address, phone_number, role, create_user, user_access) VALUES  ('Rowling', 'Potter', 'john@teamwork.com', '123456', 'male', 'developer', 'IT', 'ipaja', '090873654375', 'User', null, 'Yes');`
+  
+  pool.query(queryText1)
+    .then((res) => {
+      console.log(res);
+      pool.end();
+    })
+    .catch((err) => {
+      console.log(err);
+      pool.end();
+    });
+
+    pool.query(queryText2)
+    .then((res) => {
+      console.log(res);
+      pool.end();
+    })
+    .catch((err) => {
+      console.log(err);
+      pool.end();
+    });
+};
+
+/**
+ * Create All Tables
+ */
+const insertAllData = () => {
+  insertInitData();
+};
+
+module.exports = {
+  insertAllData,
+  insertInitData,
+};
+
+require('make-runnable');
